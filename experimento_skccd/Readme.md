15021fd9fd0e86d70effd52cbcc9d2cc44ca6b88
Pablo B <pbellino@gmail.com>
2020-06-20 13:19:47 -0300

    Comparación entre phits y mcnp

--- experimento_skccd/Readme.md
+++ experimento_skccd/Readme.md
@@ -7,3 +7,46 @@ se simula el siguiente esquema:
 
 
 >> Son dos bloques de Cu básicamente de 8 x 12 x 2 pulgadas. Entre ellos se colocó el CCD de Silicio de 9 cm x 3 cm x 675 micrómetros. Abajo de la cámara se colocó Pb de aprox 3 cm de grosor, el ancho y largo cubría en principio toda el área del Cu así que puede considerarse igual. Entre el Pb y la parte de abajo del bloque de Cu calculo una separación de unos 5 cm. La fuente de Cf252 se colocó debajo del Pb
+
+
+
+----------------------
+
+## Comparación entre MCNP y PHITS
+
+### Flujo de partículas en el Pb (celda 101)
+
+![Flujo de neutrones](espectro_neutrones.png)
+
+
+![Flujo de fotones](espectro_fotones.png)
+
+
+#### Notas:
+
+- La corrida en MCNP se hizo con todos los parámetros nucleare en default (salvo capturas analógicas, pero no influye en este ejemplo).
+
+- La corrida en MCNP es sólo con neutrones y fotones.
+
+- En MCNP no se simularon los fotones del Cf252, pues PHITS no lo simula.
+
+- La coincidencia se logra cuando se activa el generador EGS5 en PHITS para el transporte de fotones, electrones y positrones (negs=1). Por default, sólo transporta fotones (negs=-1).
+
+- Si se activa el generador de eventos para netruones de baja energía (e-mode=2) aparece un pico de neutrones de ~3keV que no está en la simulación de MCNP.
+
+- Cuando se activa negs=1 en PHITS no puede estar el material Cf252 (no existen los datos). Esto no es problema porque (a diferencia de MCNP), PHITS no requiere que exista el material Cf252 para generar una fuente de CF252.
+
+#### TODO:
+
+- Revisar el bineado entre PHITS y MCNP, los datos parecen estar desplazados en un bin de energía.
+
+- Analizar por qué aparece el pico de neutrones a baja energía con e-mode=2 en PHITS.
+
+- Mejorar la estadística.
+
+- Agregar la emisión de fotones en la fuente de Cf252 en PHITS. A mano.
+
+
+
+
+
---
+++ experimento_skccd/comparacion_espectros.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Script para leer los resultados de cada corrida de PHITS
+TODO
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import sys
+sys.path.append('/home/pablo/CinePy/')
+from modules.simulacion_modules import lee_espectro_phits_eng
+from modules.io_modules import lee_tally_E_card
+sns.set()
+
+
+if __name__ == '__main__':
+
+    archivos = [
+                'phits/track_eng_emode0_negs1.out',
+                'phits/track_eng_emode2_negs1.out',
+                'phits/track_eng_emode2_negs-1.out',
+                ]
+    labels = [
+              'e-mode=0  negs=1  ',
+              'e-mode=2  negs=1  ',
+              'e-mode=2  negs=-1  ',
+              ]
+    labels = ['PHITS - ' + s for s in labels]
+    zona = '101'
+
+    fig_n, ax_n = plt.subplots(1, 1)
+    fig_p, ax_p = plt.subplots(1, 1)
+
+    for archivo, label in zip(archivos, labels):
+        data = lee_espectro_phits_eng(archivo, zona)
+        # Puntos centrados en el bin
+        eng = (data['e-lower'] + data['e-upper']) / 2.
+        ax = {'neutron': ax_n, 'photon': ax_p}
+        for part in ['neutron', 'photon']:
+            val = data[part][:, 0]
+            std_val = val * data[part][:, 1]
+            (_, caps, _) = ax[part].errorbar(eng, val, yerr=std_val, fmt='.-',
+                                             label=label, capsize=3,
+                                             )
+            for cap in caps:
+                cap.set_markeredgewidth(1)
+
+            ax[part].set_title(part + ' en celda ' + zona)
+# ---------------------------------------------------
+    archivo = 'mcnp/skccd.o'
+    tallies = [
+               '14',
+               '514',
+              ]
+    for tal, ax in zip(tallies, [ax_n, ax_p]):
+        datos, nombre, bins = lee_tally_E_card(archivo)
+        eng = datos[tal][:, 0]
+        val = datos[tal][:, 1]
+        std_val = datos[tal][:, 2] * val
+        bin_limits = bins[tal]
+        # Calcula ancho del bin
+        bin_width = np.diff(bin_limits, axis=1)[:, 0]
+        # Normalizo con ancho del bin
+        val = val / bin_width
+        std_val = std_val / bin_width
+        (_, caps, _) = ax.errorbar(eng, val, yerr=std_val, fmt='.-',
+                                   label='MCNP', capsize=3,
+                                   )
+        for cap in caps:
+            cap.set_markeredgewidth(1)
+
+    for ax in [ax_n, ax_p]:
+        ax.set_xscale('log')
+        ax.set_yscale('log')
+        ax.set_xlabel('Energy [MeV]')
+        ax.set_ylabel('Flux [1/cm2/MeV]')
+        ax.set_xlim(1e-5, 1e2)
+        ax.set_ylim(1e-8, 1e-2)
+        ax.legend()
+
+    fig_n.savefig('espectro_neutrones.png')
+    fig_p.savefig('espectro_fotones.png')
+    plt.show()
