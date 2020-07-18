bf09622bf3562e23c7d4a87a588818cb18aeed17
Pablo B <pbellino@gmail.com>
2020-07-18 05:39:23 -0300

    Scripts para graficar mcnpn

---
+++ exp_skccd_Thinned/mcnp/Readme.md
@@ -0,0 +1,21 @@
+# Simulación del blindaje cinlíndrico para la fuente de Cf252
+
+## Modelo:
+
+1. Fuente de Cf252 emitiendo fotones y neutrones (generador de LLNL). La fuente está pegada a la pared interna del blindaje de Pb (en la coordenada y).
+2. Mesa óptica de acero inoxidable (304L) cilíndrica de 4cm de ancho y 50cm de radio.
+3. Blindaje de plomo cilíndrico con:
+   * altura = 10" 
+   * radio interno = 3"
+   * radio externo = 5"
+4. Se estima el flujo de neutrones y fotones en un paralelepípedo con Si que simula al detector:
+   - largo = 6cm
+   - altura = 2cm
+   - espesor = 2cm (en verdad tendría que haber puesto 0.02cm)
+
+
+## Resultados
+
+
+
+
---
+++ exp_skccd_Thinned/mcnp/grafica_espectros.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Script para leer los resultados de cada corrida
+TODO
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import sys
+sys.path.append('/home/pablo/CinePy/')
+from modules.io_modules import lee_tally_E_card
+sns.set()
+
+
+def escribe_espectros(archivo, tal, datos):
+    nombre_out = 'espectro_' + archivo.split('.')[0] + '_' + tal + '.dat'
+    np.savetxt(nombre_out, datos, fmt=['%1.4e', '%1.5e', '%1.4e'])
+    return None
+
+
+if __name__ == '__main__':
+
+    archivos = [
+                'Pb0.o',
+                'Pb1.o',
+                'Pb2.o',
+               ]
+    labels = [
+              'Sin mesa óptica y sin blindaje',
+              'Con mesa óptica y sin blindaje',
+              'Con mesa óptica y con blindaje',
+              ]
+    fig_n, ax_n = plt.subplots(1, 1, figsize=(8, 6))
+    fig_p, ax_p = plt.subplots(1, 1, figsize=(8, 6))
+    tals = {'neutrones': '14', 'fotones': '54'}
+    axs = [ax_n, ax_p]
+    for archivo, label in zip(archivos, labels):
+        datos, nombre, bins = lee_tally_E_card(archivo)
+        for key, ax in zip(tals.keys(), axs):
+            escribe_espectros(archivo, key, datos[tals[key]])
+            eng = datos[tals[key]][:, 0]
+            val = datos[tals[key]][:, 1]
+            std_val = datos[tals[key]][:, 2] * val
+            bin_limits = bins[tals[key]]
+            # Calcula ancho del bin
+            bin_width = np.diff(bin_limits, axis=1)[:, 0]
+            # Normalizo con ancho del bin
+            val = val / bin_width
+            std_val = std_val / bin_width
+            ax.errorbar(eng, val, yerr=std_val, label=label,
+                        drawstyle='steps-mid')
+
+    for ax in axs:
+        ax.set_xlabel('Energía [MeV]')
+        ax.set_ylabel('# particulas 1/MeV/source')
+#        ax.set_xscale('log')
+        ax.set_yscale('log')
+        ax.legend()
+    ax_p.set_xlim(5e-4, 15)
+    ax_n.set_xlim(5e-4, 25)
+
+    ax_n.set_title('Neutrones a 6" de la fuente')
+    ax_p.set_title('Fotones a 6" de la fuente')
+    fig_n.savefig('espectros_neutrones.png')
+    fig_p.savefig('espectros_fotones.png')
+
+    plt.show()
