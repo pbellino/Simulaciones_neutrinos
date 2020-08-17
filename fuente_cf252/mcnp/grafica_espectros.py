24d3cf8978b9292944636e846c6aaba28ef2e550
Pablo B <pbellino@gmail.com>
2020-08-17 19:13:26 -0300

    Se lee espectro con mctal

--- fuente_cf252/mcnp/grafica_espectros.py
+++ fuente_cf252/mcnp/grafica_espectros.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
 """
-Script para leer los resultados de cada corrida
-TODO
+Script para leer y graficar salidas de MCNP guardadas en mctal
+
+Se usa MCNPTools
 """
 
 import numpy as np
@@ -10,55 +11,53 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import sys
 sys.path.append('/home/pablo/CinePy/')
-from modules.io_modules import lee_tally_E_card
+from modules.simulacion_modules import lee_tally_E_mcnptools
 sns.set()
 
 
 def escribe_espectros(archivo, tal, datos):
-    nombre_out = 'espectro_' + archivo.split('.')[0] + '_' + tal + '.dat'
+    nombre_out = 'espectro_' + archivo.split('.')[0] + '_' + str(tal) + '.dat'
     np.savetxt(nombre_out, datos, fmt=['%1.4e', '%1.5e', '%1.4e'])
     return None
 
 
 if __name__ == '__main__':
 
-    archivos = [
-                'fuente.o',
-               ]
-    labels = [
-              'Flujo de partículas',
+    archivo = 'fuente.m'
+    tallies = [
+               11,
+               81,
               ]
+    labels = {11: 'Neutrones', 81: 'fotones'}
     fig_n, ax_n = plt.subplots(1, 1, figsize=(8, 6))
     fig_p, ax_p = plt.subplots(1, 1, figsize=(8, 6))
-    tals = {'neutrones': '11', 'fotones': '81'}
     axs = [ax_n, ax_p]
-    for archivo, label in zip(archivos, labels):
-        datos, nombre, bins = lee_tally_E_card(archivo)
-        for key, ax in zip(tals.keys(), axs):
-            # escribe_espectros(archivo, key, datos[tals[key]])
-            eng = datos[tals[key]][:, 0]
-            val = datos[tals[key]][:, 1]
-            std_val = datos[tals[key]][:, 2] * val
-            bin_limits = bins[tals[key]]
-            # Calcula ancho del bin
-            bin_width = np.diff(bin_limits, axis=1)[:, 0]
-            # Normalizo con ancho del bin
-            val = val / bin_width
-            std_val = std_val / bin_width
-            ax.errorbar(eng, val, yerr=std_val, label=label,
-                        drawstyle='steps-mid')
+    for tally, ax in zip(tallies, axs):
+        bins, val, err = lee_tally_E_mcnptools(archivo, tally)
+        # escribe archivo de texto con espectros
+        # escribe_espectros(archivo, tally, np.c_[bins, val, err])
+        std_val = err * val
+        # Calcula ancho del bin
+        bin_width = np.diff(bins)
+        # Centrado bin
+        bin_cen = bins[:-1] + 0.5 * bin_width
+        # Normalizo con ancho del bin y descarto primer dato
+        val = val[1:] / bin_width
+        std_val = std_val[1:] / bin_width
+        ax.errorbar(bin_cen, val, yerr=std_val, label=labels[tally],
+                    drawstyle='steps-mid')
 
     for ax in axs:
         ax.set_xlabel('Energía [MeV]')
-        ax.set_ylabel('# particulas 1/MeV/source')
+        ax.set_ylabel('Particulas 1/MeV/source')
         ax.set_xscale('log')
         ax.set_yscale('log')
         ax.legend()
     ax_p.set_xlim(5e-4, 15)
     ax_n.set_xlim(1e-6, 25)
 
-    ax_n.set_title('Neutrones a 6" de la fuente')
-    ax_p.set_title('Fotones a 6" de la fuente')
+    ax_n.set_title('Espectro de neutrones')
+    ax_p.set_title('Espectro de fotones')
     fig_n.savefig('espectros_neutrones.png')
     fig_p.savefig('espectros_fotones.png')
 
