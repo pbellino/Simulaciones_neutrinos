1383d9e605b47fb2213b99cf72efa0f324ece3cf
Pablo Bellino <pbellino@gmail.com>
2021-05-05 13:24:25 -0300

    Ordeno

--- quenching_measurement/mcnp/run_mcnp.sh
+++ quenching_measurement/mcnp/run_mcnp.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
 INPUT=med_qf
+#OUTPUT=med_qf_vacio.
+#OUTPUT=med_qf_poly.
+OUTPUT=med_qf_vacio_sin_fotones_de_fuente.
 
-rm ${INPUT}.*
-mcnp6 tasks 8 i=${INPUT} n=${INPUT}.
-#mcnp6 tasks 8 i=${INPUT} n=med_qf_poly.
-#mcnp6 tasks 8 i=${INPUT} n=med_qf_vacio.
+rm ${OUTPUT}.*
+mcnp6 tasks 8 i=${INPUT} n=${OUTPUT}
 
